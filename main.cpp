c94d03384f5987d82756c255946e097c60881aba
国星 张 <2296816145@qq.com>
2020-10-20 20:58:26 +0800

    Exercise 3

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ int main()
 		cv::inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV),detectMat);
 
 		imshow("whie: in the range",detectMat);
-		imshow("frame", rFrame);
+		imshow("frame", rFrame); 
 
 		waitKey(30);
 
