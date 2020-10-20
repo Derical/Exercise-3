af2c6938ba3c5a93f696bf39c6025a572ceaa7a6
国星 张 <2296816145@qq.com>
2020-10-20 22:19:29 +0800

    添加滑动条改变二值化阈值

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 ﻿#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv2\imgproc\types_c.h>
+#include <string.h>
 using namespace cv;
 using namespace std;
+
+void threshold_Mat(int th, void* data)
+{
+	Mat src = *(Mat*)(data);
+	Mat dst;
+	threshold(src, dst, th, 255, 0);
+	imshow("threshold", dst);
+}
+
 int main()
 {
 	// ------------------------- Exercise 1 -------------------------
@@ -48,17 +59,47 @@ int main()
 	// ------------------------- Exercise 2 -------------------------
 
 	//读取图片并转换为单通道灰度
-	Mat src = cv::imread("C:\\Users\\STAR ZHANG\\Pictures\\5.jpg", IMREAD_GRAYSCALE);
-	Mat dst;
-	
-	//大津法二值化
-	//threshold(src,dst,100,255,THRESH_BINARY);
+	//Mat src = cv::imread("C:\\Users\\STAR ZHANG\\Pictures\\5.jpg", IMREAD_GRAYSCALE);
+	//Mat dst;
+	//
+	////大津法二值化
+	////threshold(src,dst,100,255,THRESH_BINARY);
 
-	//区域自适应二值化
-	adaptiveThreshold(src,dst,255,ADAPTIVE_THRESH_GAUSSIAN_C,THRESH_BINARY_INV,15,10);
+	////区域自适应二值化
+	//adaptiveThreshold(src,dst,255,ADAPTIVE_THRESH_GAUSSIAN_C,THRESH_BINARY_INV,15,10);
 
-	cv::imshow("src",src);
-	cv::imshow("dst", dst);
+	//cv::imshow("src",src);
+	//cv::imshow("dst", dst);
+	//waitKey(0);
+
+	// ------------------------- Exercise 3 -------------------------
+
+	Mat src, gray;
+	int ValueSlider = 100;
+	int maxTh = 255;
+
+	src = imread("C:\\Users\\STAR ZHANG\\Pictures\\5.jpg");
+
+	if (!src.data)
+	{
+		cout << "图像载入失败！" << endl;
+		return 0;
+	}
+	cvtColor(src, gray,CV_BGR2GRAY);
+
+	////显示原图
+	//imshow("src",src);
+	////显示灰度图
+	//imshow("gray", gray);
+
+	//创建窗口
+	//参数为自动调整大小
+	namedWindow("threshold",WINDOW_GUI_NORMAL);
+	//创建滑动条
+	char Trackername[50];
+	sprintf_s(Trackername,"二值化阈值 %d",ValueSlider);
+	createTrackbar(Trackername, "threshold", &ValueSlider, maxTh, threshold_Mat, &gray);
 	waitKey(0);
+
 	return 0;
 }
